4131460be470dbe6f6f55f4a3856c36b489d6b12
Kristofer Leifland <peoplesbeer@gmail.com>
2012-03-13 17:31:19 +0100

    Added on/off button.

--- src/se/mah/helmet/StartStopActivity.java
+++ src/se/mah/helmet/StartStopActivity.java
@@ -2,17 +2,43 @@ package se.mah.helmet;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class StartStopActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        //setContentView(R.layout.main);
-        
-        TextView textview = new TextView(this);
-        textview.setText("This is the OnOff tab");
-        setContentView(textview);
+	public static final String TAG = StartStopActivity.class.getSimpleName();
+	private ToggleButton onOff;
+	private TextView txtvStatus;
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.on_off);
+		
+		onOff = (ToggleButton) findViewById(R.id.toggleButton1);
+		onOff.setOnClickListener(new OnOffListener());
+		txtvStatus = (TextView) findViewById(R.id.textView2);
+	}
+	
+    private class OnOffListener implements OnClickListener {
+    	
+    	@Override
+    	public void onClick(View v) {
+    		if (onOff.isChecked()) {
+    			Log.d(TAG, "About to start accident detection.");
+    			Toast.makeText(getApplicationContext(), "Connecting to device...", Toast.LENGTH_SHORT).show();
+    			Toast.makeText(getApplicationContext(), "Connected!", Toast.LENGTH_SHORT).show();
+    			txtvStatus.setText("Status: Connected");
+    		} else {
+    			Log.d(TAG, "About to stop accident detection.");
+    			Toast.makeText(getApplicationContext(), "Disconnecting from device.", Toast.LENGTH_SHORT).show();
+    			txtvStatus.setText("Status: Disconnected");
+    		}
+    	}
     }
 }
