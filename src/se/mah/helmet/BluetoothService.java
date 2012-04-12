a6a1224f2d7f6757840c0434048ef08b575b6c27
Kristofer Leifland <peoplesbeer@gmail.com>
2012-04-12 08:00:14 +0200

    Created (mepty) BluetoothService class.

---
+++ src/se/mah/helmet/BluetoothService.java
@@ -0,0 +1,15 @@
+package se.mah.helmet;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class BluetoothService extends Service {
+
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
