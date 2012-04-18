d88a28feb2be6da98b1ee36fda4d846eeb20b630
TELMaH P1 Grupp 6 <m10p5376@student.mah.se>
2012-04-18 11:58:12 +0200

    Added id to Trip.

--- src/se/mah/helmet/entity/Trip.java
+++ src/se/mah/helmet/entity/Trip.java
@@ -1,10 +1,20 @@
 package se.mah.helmet.entity;
 
 public class Trip {
+	private long id;
 	private String name;
-
-	public Trip(String name) {
+	
+	public Trip(long id, String name) {
+		this.id = id;
 		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
 	}
 
 	public String getName() {
