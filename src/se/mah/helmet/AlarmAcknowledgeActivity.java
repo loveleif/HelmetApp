6b840da540ae1d5ed1c5c140d4cc534abd36412e
TELMaH P1 Grupp 6 <m10p5376@student.mah.se>
2012-04-18 14:56:14 +0200

    Fixed alarm acknowledge.

--- src/se/mah/helmet/AlarmAcknowledgeActivity.java
+++ src/se/mah/helmet/AlarmAcknowledgeActivity.java
@@ -5,6 +5,8 @@ import java.util.TimerTask;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,13 +17,27 @@ public class AlarmAcknowledgeActivity extends Activity {
 	public static final int RESULT_ALARM_CANCELLED = 100;
 	public static final int RESULT_SEND_ALARM = 90000;
 
-	private int countDown = 30000; // Time left in milliseconds
+	private long startTime;
+	private long time = 0; // Time left in milliseconds
+	private long totalTime = 30000;
 	private long period = 1000;
 	
 	private Button btnCancel;
 	private TextView txtvCountDown;
 	
-	private Timer timer;
+	private Handler handler = new Handler();
+	private Runnable update = new Runnable() {
+		public void run() {
+			time = SystemClock.uptimeMillis() - startTime;
+			if (time >= totalTime) {
+				setResult(RESULT_SEND_ALARM);
+				finish();
+			} else {
+				txtvCountDown.setText((totalTime - time) / 1000 + " seconds left to cancel.");
+				handler.postDelayed(update, period);
+			}
+		}
+	};
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -38,25 +54,14 @@ public class AlarmAcknowledgeActivity extends Activity {
             }
         });
 		
-		timer = new Timer(TAG + "Timer");
-		timer.scheduleAtFixedRate(
-				new AlarmTimerTask(), 
-				0, 
-				period);
-		
+		startTime = SystemClock.uptimeMillis();
+		handler.post(update);
 	}
 	
 	private class AlarmTimerTask extends TimerTask {
 		@Override
 		public void run() {
-			countDown -= period;
-			if (countDown <= 0) {
-				timer.cancel();
-				setResult(RESULT_SEND_ALARM);
-				finish();
-			} else {
-				txtvCountDown.setText(countDown / 1000 + " seconds left to cancel.");
-			}
+
 		}
 	}
 }
