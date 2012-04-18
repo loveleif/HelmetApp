de163b7b7664e4a640a8e85180578cd753eb92b4
TELMaH P1 Grupp 6 <m10p5376@student.mah.se>
2012-04-18 11:56:38 +0200

    Created Trip.

---
+++ src/se/mah/helmet/entity/Trip.java
@@ -0,0 +1,18 @@
+package se.mah.helmet.entity;
+
+public class Trip {
+	private String name;
+
+	public Trip(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
